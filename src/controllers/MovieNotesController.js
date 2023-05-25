0d780e7a4c0748050ce7dfbaa9fed32dd8b8853f
Bruna Porato <brunaporato@gmail.com>
2023-05-25 20:08:08 -0300

    new features for movie notes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -1,4 +1,6 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 
 class MovieNotesController {
   async create(request, response) {
@@ -31,6 +33,62 @@ class MovieNotesController {
     await knex("notes").where({ id }).delete();
 
     return response.json();
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("notes").where({id}).first();
+    const tags = await knex("tags").where({note_id: id}).orderBy("name");
+
+    if(!note) {
+      throw new AppError("Movie note not found")
+    }
+    
+    return response.json({
+      ...note,
+      tags
+    })
+  }
+
+  async index(request, response) {
+    const {title, user_id,tags} = request.query;
+
+    let notes;
+
+    if(tags) {
+      const filterTags = tags.split(",").map(tag => tag.trim());
+
+      notes = await knex("tags")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .orderBy("notes.title");
+    } else {
+      notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title");
+    }
+
+    const userTags = await knex("tags").where({user_id});
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
+
   }
 }
 
--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -6,5 +6,7 @@ const movieNotesController = new MovieNotesController();
 
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
 movieNotesRoutes.delete("/:id", movieNotesController.delete);
+movieNotesRoutes.get("/:id", movieNotesController.show);
+movieNotesRoutes.get("/", movieNotesController.index);
 
 module.exports = movieNotesRoutes;
