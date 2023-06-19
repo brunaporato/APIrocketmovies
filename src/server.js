a0a7ddfa98876b6a484dad4d416baa8f1532be96
Bruna Porato <brunaporato@gmail.com>
2023-06-19 18:30:31 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
 
 const AppError = require("./utils/AppError")
-const express = require("express");
+const routes = require("./routes");
+const uploadConfig = require("./configs/upload");
 
 const app = express();
 
-const routes = require("./routes");
-
-const uploadConfig = require("./configs/upload");
+app.use(cors());
 
 app.use(express.json());
-
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
 app.use(routes);
+
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
